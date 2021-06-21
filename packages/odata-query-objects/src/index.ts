21e54c79a974f0c2463083308fc5423faa1d1f1a
texttechne <gedankengut@gmx.de>
2021-06-22 00:34:04 +0200

    chore(build): exports & improved build for qObjects

---
+++ packages/odata-query-objects/src/index.ts
@@ -0,0 +1,8 @@
+export { QEntity, QPath } from "./QEntityModel";
+export { QEntityFactory, QEntityModel, QPropContainer } from "./QEntityFactory";
+export { QExpression } from "./QExpression";
+export { QBooleanPath } from "./QBooleanPath";
+export { QNumberPath } from "./QNumberPath";
+export { QStringPath } from "./QStringPath";
+export { QEntityPath } from "./QEntityPath";
+export { QEntityCollectionPath } from "./QEntityCollectionPath";
