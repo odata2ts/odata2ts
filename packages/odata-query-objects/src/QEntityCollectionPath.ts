accbb91f06ac4bc0259f9114a7b62f14e83ada46
texttechne <gedankengut@gmx.de>
2021-06-22 00:02:33 +0200

    feat(qObjects): add entity & entityCollection path objects

---
+++ packages/odata-query-objects/src/QEntityCollectionPath.ts
@@ -0,0 +1,18 @@
+import { QEntityModel } from "./QEntityFactory";
+import { QPathModel } from "./QEntityModel";
+
+export class QEntityCollectionPath<Type> implements QPathModel {
+  constructor(private path: string, private qEntity: Omit<QEntityModel<Type, any>, "createKey">) {
+    if (!path || !path.trim()) {
+      throw Error("Path must be supplied!");
+    }
+  }
+
+  public getPath(): string {
+    return this.path;
+  }
+
+  public getEntity() {
+    return this.qEntity;
+  }
+}
