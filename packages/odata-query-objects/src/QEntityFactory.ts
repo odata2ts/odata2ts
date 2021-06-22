56a76474f9a48c7141f57d81635002786651399e
texttechne <gedankengut@gmx.de>
2021-06-22 02:32:20 +0200

    fix(qObjects): string keys in createKey

--- packages/odata-query-objects/src/QEntityFactory.ts
+++ packages/odata-query-objects/src/QEntityFactory.ts
@@ -51,21 +51,6 @@ export type QPropContainer<TypeModel> = {
 
 export class QEntityFactory {
   /**
-   * Create the key for this entity.
-   *
-   * @param keys object specifying the id of the entity
-   * @returns id string, e.g. "number=3,application='M'"
-   */
-  static createKey<TypeModel, KeyModel extends keyof TypeModel>(keys: { [Key in KeyModel]: TypeModel[Key] }): string {
-    return Object.entries(keys)
-      .map(([key, value]) => {
-        // const val =
-        return `${key}=${value}`;
-      })
-      .join(",");
-  }
-
-  /**
    * Factory function to create a QEntity based on a given type (TypeModel) in a type-safe manner.
    * Possible properties are checked via automatic type mapping.
    *
@@ -80,7 +65,15 @@ export class QEntityFactory {
     return {
       ...props,
       entityName: entityName,
-      createKey: this.createKey,
+      createKey: (keys: { [Key in KeyModel]: TypeModel[Key] }): string => {
+        return Object.entries(keys)
+          .map(([key, value]) => {
+            const prop = props[key as keyof TypeModel];
+            const val = prop && prop instanceof QStringPath ? `'${value}'` : value;
+            return key + "=" + val;
+          })
+          .join(",");
+      },
     };
   }
 }
