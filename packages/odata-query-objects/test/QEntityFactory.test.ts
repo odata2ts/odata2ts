17be3a11d001a8b00344c53e04aa80ca8ee53c70
texttechne <gedankengut@gmx.de>
2021-06-22 02:30:49 +0200

    refactor: restructrue QEntityFactory test

--- packages/odata-query-objects/test/QEntityFactory.test.ts
+++ packages/odata-query-objects/test/QEntityFactory.test.ts
@@ -4,62 +4,93 @@ import { QDateTimeOffsetPath } from "../src/date-time-v4/QDateTimeOffsetPath";
 import { QTimeOfDayPath } from "../src/date-time-v4/QTimeOfDayPath";
 import { DateString, DateTimeOffsetString, TimeOfDayString } from "../src/ODataTypes";
 
-test("no test here - this file only needs to be type checked", () => {});
-
-interface Test2 {
+interface SimpleEntity {
   name: string;
 }
 
-interface Test {
+interface ComplexEntity {
   x: number;
   y?: string;
   Z: boolean;
   az: DateString;
   bz?: TimeOfDayString;
   cz: DateTimeOffsetString;
-  xy?: Test2;
-  xx: Array<Test2>;
+  xy?: SimpleEntity;
+  xx: Array<SimpleEntity>;
 }
 
-const qTesterFailing = QEntityFactory.create<Test2, "name">("test2", {
-  // @ts-expect-error => wrong type
-  name: new QDatePath("name"),
+describe("QEntityFactory tests", () => {
+  // Typing Test: Expect error for wrong typing
+  QEntityFactory.create<SimpleEntity, "name">("test2", {
+    // @ts-expect-error => wrong type, should be string
+    name: new QDatePath("name"),
+  });
+
+  const qSimple = QEntityFactory.create<SimpleEntity, "name">("test2", {
+    name: new QStringPath("name"),
+    // Typing Test: Expect error for unknown members
+    // @ts-expect-error => unknown member
+    whoAmI: new QStringPath("whoAmI"),
+  });
+
+  const qComplex = QEntityFactory.create<ComplexEntity, "x" | "Z">("test", {
+    x: new QNumberPath("x"),
+    y: new QStringPath("y"),
+    Z: new QBooleanPath("Z"),
+    az: new QDatePath("az"),
+    bz: new QTimeOfDayPath("bz"),
+    cz: new QDateTimeOffsetPath("cz"),
+    xy: new QEntityPath("xy", qSimple),
+    xx: new QEntityCollectionPath("xx", qSimple),
+  });
+
+  test("create simple key", () => {
+    // @ts-expect-error
+    qSimple.createKey({ city: "Test" });
+    // @ts-expect-error
+    qSimple.createKey({ name: 123 });
+
+    const result = qSimple.createKey({ name: "Horst" });
+    expect(result).toBe("name='Horst'");
+  });
+
+  test("create complex key", () => {
+    // @ts-expect-error
+    qComplex.createKey({});
+    // @ts-expect-error
+    qComplex.createKey({ x: 2 });
+    // @ts-expect-error
+    qComplex.createKey({ x: "2", Z: false });
+
+    const result = qComplex.createKey({ x: 123, Z: false });
+    expect(result).toBe("x=123,Z=false");
+  });
+
+  test("simple prop", () => {
+    // @ts-expect-error
+    qSimple.name.eq(3);
+
+    const result = qSimple.name.eq("hi").toString();
+    expect(result).toBe("name eq 'hi'");
+  });
+
+  test("entity path", () => {
+    const xyEntity = qComplex.xy.getEntity();
+
+    //@ts-expect-error
+    xyEntity.createKey({ name: 3 });
+
+    const result = xyEntity.name.startsWith("Hi").toString();
+    expect(result).toBe("startswith(name,'Hi')");
+  });
+
+  test("entity collection path", () => {
+    const xxEntity = qComplex.xx.getEntity();
+
+    //@ts-expect-error
+    xxEntity.createKey({ name: 3 });
+
+    const result = xxEntity.name.startsWith("Hi").toString();
+    expect(result).toBe("startswith(name,'Hi')");
+  });
 });
-
-const qTester2 = QEntityFactory.create<Test2, "name">("test2", {
-  name: new QStringPath("name"),
-  // @ts-expect-error => unknown member
-  whoAmI: new QStringPath("test"),
-});
-
-const qTester = QEntityFactory.create<Test, "x" | "Z">("test", {
-  x: new QNumberPath("x"),
-  y: new QStringPath("y"),
-  Z: new QBooleanPath("Z"),
-  az: new QDatePath("az"),
-  bz: new QTimeOfDayPath("bz"),
-  cz: new QDateTimeOffsetPath("cz"),
-  xy: new QEntityPath("xy", qTester2),
-  xx: new QEntityCollectionPath("xx", qTester2),
-});
-
-qTester.x.eq(3).and(qTester.x.add(5.3).ceiling().lt(10));
-// @ts-expect-error
-qTester.x.eq("3");
-
-qTester.y.eq("hi").or(qTester.y.startsWith("hello"));
-// @ts-expect-error
-qTester.y.eq(3);
-
-qTester.createKey({ x: 2, Z: false });
-// @ts-expect-error
-qTester.createKey({});
-// @ts-expect-error
-qTester.createKey({ x: 2 });
-// @ts-expect-error
-qTester.createKey({ x: "2", Z: false });
-
-const xyEntity = qTester.xy.getEntity();
-xyEntity.name.endsWith("be!");
-//@ts-expect-error
-xyEntity.createKey({ name: 3 });
