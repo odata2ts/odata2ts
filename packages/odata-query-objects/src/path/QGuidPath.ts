b9cc9acdff0be164779ef5d105cf45f057a996e8
texttechne <gedankengut@gmx.de>
2021-07-02 21:33:16 +0200

    fix(qObjects): QGuidPath must return expressions

--- packages/odata-query-objects/src/path/QGuidPath.ts
+++ packages/odata-query-objects/src/path/QGuidPath.ts
@@ -1,3 +1,4 @@
+import { QExpression } from "./QExpression";
 import { QPathModel } from "./QPathModel";
 import { StandardFilterOperators } from "../odata/ODataModel";
 
@@ -16,33 +17,37 @@ export class QGuidPath implements QPathModel {
     return `${this.getPath()} ${operator} ${value}`;
   }
 
+  private buildBuiltInExpression(operator: StandardFilterOperators, value: string) {
+    return new QExpression(this.buildBuiltInOp(operator, value));
+  }
+
   public equals(value: string) {
-    return this.buildBuiltInOp(StandardFilterOperators.EQUALS, value);
+    return this.buildBuiltInExpression(StandardFilterOperators.EQUALS, value);
   }
   public eq = this.equals;
 
   public notEquals(value: string) {
-    return this.buildBuiltInOp(StandardFilterOperators.NOT_EQUALS, value);
+    return this.buildBuiltInExpression(StandardFilterOperators.NOT_EQUALS, value);
   }
   public ne = this.notEquals;
 
   public lowerThan(value: string) {
-    return this.buildBuiltInOp(StandardFilterOperators.LOWER_THAN, value);
+    return this.buildBuiltInExpression(StandardFilterOperators.LOWER_THAN, value);
   }
   public lt = this.lowerThan;
 
   public lowerEquals(value: string) {
-    return this.buildBuiltInOp(StandardFilterOperators.LOWER_EQUALS, value);
+    return this.buildBuiltInExpression(StandardFilterOperators.LOWER_EQUALS, value);
   }
   public le = this.lowerEquals;
 
   public greaterThan(value: string) {
-    return this.buildBuiltInOp(StandardFilterOperators.GREATER_THAN, value);
+    return this.buildBuiltInExpression(StandardFilterOperators.GREATER_THAN, value);
   }
   public gt = this.greaterThan;
 
   public greaterEquals(value: string) {
-    return this.buildBuiltInOp(StandardFilterOperators.GREATER_EQUALS, value);
+    return this.buildBuiltInExpression(StandardFilterOperators.GREATER_EQUALS, value);
   }
   public ge = this.greaterEquals;
 }
