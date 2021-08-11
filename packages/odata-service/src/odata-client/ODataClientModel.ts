a5597d1a1b3a3d8faceec8724ce94221b6bbeaa9
texttechne <gedankengut@gmx.de>
2021-08-11 12:54:07 +0200

    feat(odata-service): generic inteface for OData client

--- packages/odata-service/src/odata-client/ODataClientModel.ts
+++ packages/odata-service/src/odata-client/ODataClientModel.ts
@@ -1,6 +1,8 @@
 export interface ODataClient {
+  post<T>(url: string, data: T): Promise<T>;
   get<T>(url: string): Promise<T>;
   put<T>(url: string, data: T): Promise<any>;
+  update<T>(url: string, data: T): Promise<any>;
   patch<T>(url: string, data: Partial<T>): Promise<void>;
   delete<T>(url: string): Promise<void>;
 }
