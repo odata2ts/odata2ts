d9e4f647e940bf3727989a0c1e3f0e1825df733b
texttechne <gedankengut@gmx.de>
2021-08-11 12:52:52 +0200

    refactor(uri-builder): remove byId function
    BREAKING CHANGE: public API has changed by removing byId()

--- packages/odata-uri-builder/src/ODataUriBuilder.ts
+++ packages/odata-uri-builder/src/ODataUriBuilder.ts
@@ -1,10 +1,10 @@
-import { QEntityModel, QStringPath } from "@odata2ts/odata-query-objects";
+import { QEntityModel } from "@odata2ts/odata-query-objects";
 import { ODataUriBuilderBase, ODataUriBuilderConfig } from "./internal";
 
 /**
  * Create an OData URI string in a typesafe way by facilitating generated query objects.
  */
-export class ODataUriBuilder<T, K extends keyof T> extends ODataUriBuilderBase<T> {
+export class ODataUriBuilder<T> extends ODataUriBuilderBase<T> {
   /**
    * Create an UriBuilder by passing in a query object, which already contains the base path
    * to the OData service & the given entity.
@@ -21,8 +21,8 @@ export class ODataUriBuilder<T, K extends keyof T> extends ODataUriBuilderBase<T
    * @param config optionally pass a configuration
    * @returns a UriBuilder
    */
-  static create<T, K extends keyof T>(path: string, qEntity: QEntityModel<T>, config?: ODataUriBuilderConfig) {
-    return new ODataUriBuilder<T, K>(path, qEntity, config);
+  static create<T>(path: string, qEntity: QEntityModel<T>, config?: ODataUriBuilderConfig) {
+    return new ODataUriBuilder<T>(path, qEntity, config);
   }
 
   protected readonly entity: QEntityModel<T>;
@@ -31,24 +31,6 @@ export class ODataUriBuilder<T, K extends keyof T> extends ODataUriBuilderBase<T
   private constructor(path: string, qEntity: QEntityModel<T>, config?: ODataUriBuilderConfig) {
     super(path, qEntity, config);
     this.entity = qEntity;
-  }
-
-  /**
-   * Get a specific entity by its key / id.
-   *
-   * @param keys the key object
-   * @returns this query builder
-   */
-  public byKey(keys: { [Key in K]: T[Key] }) {
-    this.key = Object.entries<T[K]>(keys)
-      .map(([key, value]) => {
-        const prop = this.entity[key as K];
-        const val = prop && prop instanceof QStringPath ? `'${value}'` : value;
-        return key + "=" + val;
-      })
-      .join(",");
-
-    return this;
   }
 
   /**
