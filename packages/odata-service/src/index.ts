2b6fd169d5bf6dc63e1ab8b9df573ad276479e54
texttechne <gedankengut@gmx.de>
2021-08-13 15:48:14 +0200

    feat(service): url helper methods

--- packages/odata-service/src/index.ts
+++ packages/odata-service/src/index.ts
@@ -2,3 +2,4 @@ export { ODataService } from "./ODataService";
 export { EntitySetService } from "./EntitySetService";
 export { EntityTypeService } from "./EntityTypeService";
 export { CollectionService } from "./CollectionService";
+export * from "./helper/UrlHelper";
