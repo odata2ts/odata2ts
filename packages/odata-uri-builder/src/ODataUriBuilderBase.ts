5f4fcfb85d905e53fde0f636830e924c4be1da89
texttechne <gedankengut@gmx.de>
2021-08-16 10:08:43 +0200

    fix(uri-builder): adapt to QEntityCollectionPath

--- packages/odata-uri-builder/src/ODataUriBuilderBase.ts
+++ packages/odata-uri-builder/src/ODataUriBuilderBase.ts
@@ -13,7 +13,7 @@ export interface ODataUriBuilderConfig {
   unencoded?: boolean;
 }
 
-type EntityExtractor<T> = T extends QEntityPath<infer E> ? E : T extends QCollectionPath<infer E> ? E : never;
+type EntityExtractor<T> = T extends QEntityPath<infer E> ? E : T extends QEntityCollectionPath<infer E> ? E : never;
 type ExtractPropertyNamesOfType<T, S> = { [K in keyof T]: T[K] extends S ? K : never }[keyof T];
 type ExpandType<T> = ExtractPropertyNamesOfType<
   QEntityModel<T>,
