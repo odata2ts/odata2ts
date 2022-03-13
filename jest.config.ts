3acef8b83b2625579bbce4a967724e884c39c358
texttechne <gedankengut@gmx.de>
2022-03-14 00:42:27 +0100

    fix(test): collect code coverage from src folders, thus exhibiting untested code

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,5 @@
 module.exports = {
   projects: ["<rootDir>/packages/*/jest.config.ts"],
   coverageDirectory: "<rootDir>/coverage/",
-  // collectCoverageFrom: ["**/*.{ts,tsx}", "!**/*.d.ts", "!<rootDir>/lib/"],
+  collectCoverageFrom: ["**/src/**/*.{ts,tsx}"],
 };
--- packages/odata-query-objects/jest.config.ts
+++ packages/odata-query-objects/jest.config.ts
@@ -4,6 +4,7 @@ const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
   coverageDirectory: "coverage",
+  collectCoverageFrom: ["src/**/*.ts"],
   globals: {
     "ts-jest": {
       tsconfig: "tsconfig.test.json",
