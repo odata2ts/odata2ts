ad9d3f2536828363ef153f54e31f3882089ee3a1
texttechne <gedankengut@gmx.de>
2022-04-04 07:20:11 +0200

    feat(test): app test

--- packages/odata2model/test/app.test.ts
+++ packages/odata2model/test/app.test.ts
@@ -1,5 +1,131 @@
+import { ODataModelBuilder, ODataVersion } from "./data-model/builder/ODataModelBuilder";
+
+import { SERVICE_NAME } from "./generator/EntityBasedGenerationTests";
+import { EmitModes, Modes, RunOptions } from "../src/OptionModel";
+import { runApp } from "../src/app";
+import * as ProjectManager from "../src/project/ProjectManager";
+import * as Generator from "../src/generator";
+
+jest.mock("fs-extra");
+jest.mock("ts-morph");
+jest.mock("../src/generator");
+
 describe("App Test", () => {
-  test("Smoke Test", () => {
-    expect(true).toBeTruthy();
-  })
-})
+  let runOptions: RunOptions;
+  let odataBuilder: ODataModelBuilder;
+  let createPmSpy: jest.SpyInstance;
+  let pmSpy: ProjectManager.ProjectManager;
+
+  beforeAll(async () => {
+    // @ts-ignore
+    pmSpy = {
+      createModelFile: jest.fn(),
+      createQObjectFile: jest.fn(),
+      cleanServiceDir: jest.fn(),
+      createMainServiceFile: jest.fn(),
+      createServiceFile: jest.fn(),
+      writeFiles: jest.fn(),
+    };
+    // @ts-ignore
+    createPmSpy = jest.spyOn(ProjectManager, "createProjectManager").mockResolvedValue(pmSpy);
+  });
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+
+    odataBuilder = new ODataModelBuilder(ODataVersion.V4, SERVICE_NAME);
+    runOptions = {
+      mode: Modes.models,
+      emitMode: EmitModes.ts,
+      output: "ignore",
+      prettier: false,
+      debug: false,
+      modelPrefix: "",
+      modelSuffix: "",
+    };
+  });
+
+  function doRunApp() {
+    return runApp(odataBuilder.getModel(), runOptions);
+  }
+
+  test("App: generate only models", async () => {
+    // given preset runOptions
+    // when running the app
+    await doRunApp();
+
+    // then project manager was called with our arguments
+    expect(createPmSpy.mock.calls[0][0]).toMatchObject({
+      model: "TesterModel",
+      qObject: "QTester",
+      service: "TesterService",
+    });
+    expect(createPmSpy.mock.calls[0][1]).toBe(runOptions.output);
+    expect(createPmSpy.mock.calls[0][2]).toBe(runOptions.emitMode);
+    expect(createPmSpy.mock.calls[0][3]).toBe(runOptions.prettier);
+
+    // then only generateModels was called
+    expect(Generator.generateModels).toHaveBeenCalled();
+    expect(Generator.generateQueryObjects).not.toHaveBeenCalled();
+    expect(Generator.generateServices).not.toHaveBeenCalled();
+
+    // then files should have been written
+    expect(pmSpy.writeFiles).toHaveBeenCalled();
+  });
+
+  test("App: generate also QObjects", async () => {
+    // given
+    runOptions.mode = Modes.qobjects;
+    runOptions.output = "testing";
+    runOptions.emitMode = EmitModes.js_dts;
+    runOptions.prettier = true;
+
+    // when running the app
+    await doRunApp();
+
+    // then project manager was called with our arguments
+    expect(createPmSpy.mock.calls[0][1]).toBe("testing");
+    expect(createPmSpy.mock.calls[0][2]).toBe(EmitModes.js_dts);
+    expect(createPmSpy.mock.calls[0][3]).toBe(true);
+
+    // then generateModels & generateQObjects was called
+    expect(Generator.generateModels).toHaveBeenCalled();
+    expect(Generator.generateQueryObjects).toHaveBeenCalled();
+    expect(Generator.generateServices).not.toHaveBeenCalled();
+
+    // then files should have been written
+    expect(pmSpy.writeFiles).toHaveBeenCalled();
+  });
+
+  test("App: generate also services", async () => {
+    // given
+    runOptions.mode = Modes.service;
+
+    // when running the app
+    await doRunApp();
+
+    // then all generators have been called
+    expect(Generator.generateModels).toHaveBeenCalled();
+    expect(Generator.generateQueryObjects).toHaveBeenCalled();
+    expect(Generator.generateServices).toHaveBeenCalled();
+
+    // then files should have been written
+    expect(pmSpy.writeFiles).toHaveBeenCalled();
+  });
+
+  test("App: generate all", async () => {
+    // given
+    runOptions.mode = Modes.all;
+
+    // when running the app
+    await doRunApp();
+
+    // then all generators have been called
+    expect(Generator.generateModels).toHaveBeenCalled();
+    expect(Generator.generateQueryObjects).toHaveBeenCalled();
+    expect(Generator.generateServices).toHaveBeenCalled();
+
+    // then files should have been written
+    expect(pmSpy.writeFiles).toHaveBeenCalled();
+  });
+});
