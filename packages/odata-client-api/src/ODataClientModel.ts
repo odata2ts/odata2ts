04ce52e93b89a7974ee7e113d1fd4ed09ec50bb5
texttechne <gedankengut@gmx.de>
2022-05-14 18:11:18 +0200

    refactor(clientApi): Add documentation & optional merge method for V2

--- packages/odata-client-api/src/ODataClientModel.ts
+++ packages/odata-client-api/src/ODataClientModel.ts
@@ -1,10 +1,57 @@
 import { ODataResponse } from "./ODataResponseModel";
 
 export interface ODataClient<RequestConfig = any> {
+  /**
+   * Create a model or collection entry.
+   *
+   * @param url
+   * @param data
+   * @param requestConfig
+   */
   post<T, ResponseModel>(url: string, data: T, requestConfig?: RequestConfig): ODataResponse<ResponseModel>;
+
   get<T>(url: string, requestConfig?: RequestConfig): ODataResponse<T>;
+
+  /**
+   * Replace a model.
+   *
+   * @param url
+   * @param data
+   * @param requestConfig
+   */
   put<T>(url: string, data: T, requestConfig?: RequestConfig): ODataResponse<void>;
+
+  /**
+   * Partially update a model.
+   *
+   * @param url
+   * @param data
+   * @param requestConfig
+   */
   patch<T>(url: string, data: Partial<T>, requestConfig?: RequestConfig): ODataResponse<void>;
+
+  /**
+   * OData V2 only feature.
+   * Historically, PATCH method was not part of the official HTTP spec, when V1 & V2 were specified;
+   * but use of PATCH was already envisioned {@link https://www.odata.org/documentation/odata-version-2-0/operations/}.
+   * V3 and all following versions, specify use of PATCH method.
+   *
+   * If implemented, this method wil be used instead of patch to partially update models.
+   * Use case: You want to reuse this client and one server can only handle MERGE,
+   * but not PATCH http requests.
+   *
+   * @param url
+   * @param data
+   * @param requestConfig
+   */
+  merge?<T>(url: string, data: Partial<T>, requestConfig?: RequestConfig): ODataResponse<void>;
+
+  /**
+   * Delete a model or collection.
+   *
+   * @param url
+   * @param requestConfig
+   */
   delete(url: string, requestConfig?: RequestConfig): ODataResponse<void>;
 
   // TODO --- OData Feature Matrix --- TODO
