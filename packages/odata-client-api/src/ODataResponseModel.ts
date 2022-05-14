89d7bece3637228d10df34d6494dd2ba3f0b45db
texttechne <gedankengut@gmx.de>
2022-05-14 18:10:46 +0200

    refactor(clientApi): Refactor response model & remove individual response types, e.g. collection or raw data
    BREAKING CHANGE: refactored interfaces

--- packages/odata-client-api/src/ODataResponseModel.ts
+++ packages/odata-client-api/src/ODataResponseModel.ts
@@ -1,8 +1,4 @@
-/**
- * Wrapping response instance, containing status code info, headers
- * and the response body.
- */
-export type ODataResponse<T> = Promise<{
+export interface HttpResponseModel<T> {
   /** status code, e.g. 200 or 404 */
   status: number;
   /** status text, e.g. 200 = OK or 404 = Not Found */
@@ -10,37 +6,10 @@ export type ODataResponse<T> = Promise<{
   headers: { [key: string]: string };
   // config?: any;
   data: T;
-}>;
-
-/**
- * Response to a collection query.
- */
-export interface ODataCollectionResponse<T> {
-  "@odata.context": string;
-  "@odata.count"?: number;
-  value: Array<T>;
 }
 
 /**
- * Response to query for an EntityType or ComplexType
- *
- * Result object is directly returned.
- * Additional context attribute is merged into result object.
+ * Wrapping response instance, containing status code info, headers
+ * and the response body.
  */
-export type ODataModelResponse<T> = T & {
-  "@odata.context": string;
-};
-
-/**
- * Response to a value query on a property.
- */
-export interface ODataValueResponse<T> {
-  "@odata.context": string;
-  value: T;
-}
-
-/**
- * Response to query for a raw value, by appending '/$value'
- * to the path of a property.
- */
-export type ODataRawResponse<T> = T;
+export type ODataResponse<T> = Promise<HttpResponseModel<T>>;
