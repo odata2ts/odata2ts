63342135a41452146decc6cdc5c04f96865999e8
texttechne <gedankengut@gmx.de>
2022-07-06 15:21:01 +0200

    feat: add option to enable automatic csrf token handling

--- packages/axios-odata-client/src/AxiosODataClient.ts
+++ packages/axios-odata-client/src/AxiosODataClient.ts
@@ -5,11 +5,80 @@ import { RequestError } from "./ODataRequestErrorModel";
 
 export type ErrorMessageRetriever = (error: AxiosError) => string | null | undefined;
 
-export class AxiosODataClient implements ODataClient<AxiosRequestConfig> {
-  private client: AxiosInstance;
+export interface ClientOptions {
+  useCsrfProtection?: boolean;
+  csrfTokenFetchUrl?: string;
+}
 
-  constructor(private getErrorMessage: ErrorMessageRetriever, config?: AxiosRequestConfig) {
+export class AxiosODataClient implements ODataClient<AxiosRequestConfig> {
+  private readonly client: AxiosInstance;
+  private csrfToken: string | undefined;
+
+  constructor(
+    private getErrorMessage: ErrorMessageRetriever,
+    config?: AxiosRequestConfig,
+    private clientOptions?: ClientOptions
+  ) {
     this.client = axios.create(config);
+
+    if (clientOptions?.useCsrfProtection) {
+      this.setupSecurityTokenInterceptors();
+    }
+  }
+
+  private setupSecurityTokenInterceptors() {
+    this.client.interceptors.request.use(
+      async (config: AxiosRequestConfig & { "skip-csrf-token-handling"?: boolean }) => {
+        if (config["skip-csrf-token-handling"]) {
+          return config;
+        }
+        if (config.method != null && ["POST", "PUT", "DELETE"].includes(config.method.toUpperCase())) {
+          await this.setupSecurityToken();
+          if (config.headers == null) {
+            config.headers = {};
+          }
+
+          if (this.csrfToken != null) {
+            config.headers["x-csrf-token"] = this.csrfToken;
+          }
+        }
+        return config;
+      }
+    );
+
+    this.client.interceptors.response.use(undefined, (error: Error | AxiosError) => {
+      if (
+        axios.isAxiosError(error) &&
+        error.config != null &&
+        error.response?.status === 403 &&
+        error.response?.headers?.["x-csrf-token"] === "Required"
+      ) {
+        // sap csrf token expires, let's reset it and refetch
+        this.csrfToken = undefined;
+
+        return this.client.request(error.config);
+      }
+      return Promise.reject(error);
+    });
+  }
+
+  private async setupSecurityToken() {
+    if (this.csrfToken == null) {
+      this.csrfToken = await this.fetchSecurityToken();
+    }
+  }
+
+  private async fetchSecurityToken(): Promise<string> {
+    const fetchUrl = this.clientOptions?.csrfTokenFetchUrl ?? "/";
+    const response = await this.client.get(fetchUrl, {
+      headers: {
+        "x-csrf-token": "Fetch",
+      },
+      // @ts-ignore
+      "skip-csrf-token-handling": true,
+    });
+
+    return response.headers["x-csrf-token"];
   }
 
   private handleError<T>(instance: AxiosPromise): Promise<AxiosResponse<T>> {
@@ -39,19 +108,23 @@ export class AxiosODataClient implements ODataClient<AxiosRequestConfig> {
     return error;
   }
 
-  post<T, ResponseModel>(url: string, data: T, requestConfig?: AxiosRequestConfig): AxiosPromise<ResponseModel> {
+  public getInstance() {
+    return this.client;
+  }
+
+  public post<T, ResponseModel>(url: string, data: T, requestConfig?: AxiosRequestConfig): AxiosPromise<ResponseModel> {
     return this.handleError(this.client.post(url, data, requestConfig));
   }
-  get<T>(url: string, requestConfig?: AxiosRequestConfig): AxiosPromise<T> {
+  public get<T>(url: string, requestConfig?: AxiosRequestConfig): AxiosPromise<T> {
     return this.handleError(this.client.get(url, requestConfig));
   }
-  put<T>(url: string, data: T, requestConfig?: AxiosRequestConfig): AxiosPromise<void> {
+  public put<T>(url: string, data: T, requestConfig?: AxiosRequestConfig): AxiosPromise<void> {
     return this.handleError(this.client.put(url, data, requestConfig));
   }
-  patch<T>(url: string, data: Partial<T>, requestConfig?: AxiosRequestConfig): AxiosPromise<void> {
+  public patch<T>(url: string, data: Partial<T>, requestConfig?: AxiosRequestConfig): AxiosPromise<void> {
     return this.handleError(this.client.patch(url, data, requestConfig));
   }
-  delete(url: string, requestConfig?: AxiosRequestConfig): AxiosPromise<void> {
+  public delete(url: string, requestConfig?: AxiosRequestConfig): AxiosPromise<void> {
     return this.handleError(this.client.delete(url, requestConfig));
   }
 }
