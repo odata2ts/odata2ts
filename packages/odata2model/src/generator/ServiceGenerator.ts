caddb306274c0ea2c32695b4323b829a9be3cba5
texttechne <gedankengut@gmx.de>
2022-07-06 15:59:28 +0200

    feat(odata2model): actions & function on collection level
    SAP RAP specialty: instead of unbound actions / functions we have static methods which are bound to the entity and hence appear on the CollectionService; this specialty is detected by virtue of the first parameter which is a collection

--- packages/odata2model/src/generator/ServiceGenerator.ts
+++ packages/odata2model/src/generator/ServiceGenerator.ts
@@ -249,6 +249,7 @@ class ServiceGenerator {
       if (model.modelType === ModelTypes.EntityType) {
         importContainer.addFromService(entitySetServiceType);
 
+        const collectionOperations = this.dataModel.getOperationTypeByBinding(`Collection(${model.name})`);
         const isSingleKey = model.keys.length === 1;
         const exactKeyType = `{ ${model.keys.map((k) => `${k.odataName}: ${this.sanitizeType(k.type)}`).join(", ")} }`;
         const keyType = `${isSingleKey ? this.sanitizeType(model.keys[0].type) + " | " : ""}${exactKeyType}`;
@@ -269,6 +270,7 @@ class ServiceGenerator {
               statements: [`super(client, path, ${firstCharLowerCase(model.qName)}, ${serviceName}, ${keySpec});`],
             },
           ],
+          methods: [...this.generateBoundOperations(collectionOperations, importContainer)],
         });
       }
 
