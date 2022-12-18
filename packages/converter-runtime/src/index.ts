0a41b9d4f884228f94b88aed795d258fc16d1bd0
texttechne <gedankengut@gmx.de>
2022-12-18 20:47:44 +0100

    chore: remove converters from this repository => moved to @odata2ts/converter repo

--- packages/converter-runtime/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./ConverterModels";
-export * from "./loadConverters";
-export * from "./chainConverters";
-export { getIdentityConverter } from "./IdentityConverter";
-export { ChainedConverter } from "./ChainedConverter";
