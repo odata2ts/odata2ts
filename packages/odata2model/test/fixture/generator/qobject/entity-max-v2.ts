703c828b0958d8a999d25c9ffdfe9f31512f35dd
texttechne <gedankengut@gmx.de>
2022-12-31 05:05:42 +0100

    feat(odata2ts): use new number paths for V2

--- packages/odata2model/test/fixture/generator/qobject/entity-max-v2.ts
+++ packages/odata2model/test/fixture/generator/qobject/entity-max-v2.ts
@@ -13,9 +13,11 @@ import {
   QGuidV2Param,
   QGuidV2Path,
   QId,
-  QNumberCollection,
   QNumberParam,
-  QNumberPath,
+  QNumberV2Collection,
+  QNumberV2Path,
+  QStringNumberV2Collection,
+  QStringNumberV2Path,
   QStringV2Collection,
   QStringV2Path,
   QTimeV2Collection,
@@ -28,20 +30,20 @@ import { BookId } from "./TesterModel";
 
 export class QBook extends QueryObject {
   public readonly id = new QGuidV2Path(this.withPrefix("id"));
-  public readonly id2 = new QNumberPath(this.withPrefix("id2"));
+  public readonly id2 = new QNumberV2Path(this.withPrefix("id2"));
   public readonly id3 = new QBooleanPath(this.withPrefix("id3"));
   public readonly truth = new QBooleanPath(this.withPrefix("requiredOption"));
   public readonly time = new QTimeV2Path(this.withPrefix("time"));
   public readonly optionalDate = new QDateTimeV2Path(this.withPrefix("optionalDate"));
   public readonly dateTimeOffset = new QDateTimeOffsetV2Path(this.withPrefix("dateTimeOffset"));
-  public readonly testByte = new QNumberPath(this.withPrefix("testByte"));
-  public readonly testSByte = new QNumberPath(this.withPrefix("testSByte"));
-  public readonly testInt16 = new QNumberPath(this.withPrefix("testInt16"));
-  public readonly testInt32 = new QNumberPath(this.withPrefix("testInt32"));
-  public readonly testInt64 = new QNumberPath(this.withPrefix("testInt64"));
-  public readonly testSingle = new QNumberPath(this.withPrefix("testSingle"));
-  public readonly testDouble = new QNumberPath(this.withPrefix("testDouble"));
-  public readonly testDecimal = new QNumberPath(this.withPrefix("TestDecimal"));
+  public readonly testByte = new QStringNumberV2Path(this.withPrefix("testByte"));
+  public readonly testSByte = new QStringNumberV2Path(this.withPrefix("testSByte"));
+  public readonly testInt16 = new QNumberV2Path(this.withPrefix("testInt16"));
+  public readonly testInt32 = new QNumberV2Path(this.withPrefix("testInt32"));
+  public readonly testInt64 = new QStringNumberV2Path(this.withPrefix("testInt64"));
+  public readonly testSingle = new QStringNumberV2Path(this.withPrefix("testSingle"));
+  public readonly testDouble = new QStringNumberV2Path(this.withPrefix("testDouble"));
+  public readonly testDecimal = new QStringNumberV2Path(this.withPrefix("TestDecimal"));
   public readonly testBinary = new QBinaryPath(this.withPrefix("testBinary"));
   public readonly testAny = new QStringV2Path(this.withPrefix("testAny"));
   public readonly multipleIds = new QCollectionPath(this.withPrefix("multipleIds"), () => QGuidV2Collection);
@@ -56,8 +58,11 @@ export class QBook extends QueryObject {
     this.withPrefix("multipleDateTimeOffsets"),
     () => QDateTimeOffsetV2Collection
   );
-  public readonly multipleInt16 = new QCollectionPath(this.withPrefix("multipleInt16"), () => QNumberCollection);
-  public readonly multipleDecimals = new QCollectionPath(this.withPrefix("multipleDecimals"), () => QNumberCollection);
+  public readonly multipleInt16 = new QCollectionPath(this.withPrefix("multipleInt16"), () => QNumberV2Collection);
+  public readonly multipleDecimals = new QCollectionPath(
+    this.withPrefix("multipleDecimals"),
+    () => QStringNumberV2Collection
+  );
   public readonly multipleBinaries = new QCollectionPath(this.withPrefix("multipleBinaries"), () => QBinaryCollection);
 }
 
