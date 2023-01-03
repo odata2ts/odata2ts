8a883b11d97e62aee906538fb13b8863df78b4cf
texttechne <gedankengut@gmx.de>
2023-01-03 23:10:17 +0100

    test(example-cap): V2 operations

--- examples/bookshop/int-test/v2/OperationFunctionalityV2.test.ts
+++ examples/bookshop/int-test/v2/OperationFunctionalityV2.test.ts
@@ -1,19 +1,26 @@
-import { BooksModel } from "../src/catalog/CatalogModel";
-import { catalogService } from "./services";
+import { BooksModel } from "../../src/catV2/CatV2Model";
+import { catalogV2Service } from "./servicesV2";
 
-describe("CAP V4 Integration Testing: Operation Capabilities", () => {
-  const testService = catalogService;
+describe("CAP V2 Integration Testing: Operation Capabilities", () => {
+  const testService = catalogV2Service;
 
-  const BOOK_ZERO: Omit<BooksModel, "image" | "createdAt" | "modifiedAt"> = {
+  const BOOK_ZERO: Omit<
+    BooksModel,
+    "currency" | "texts" | "author" | "localized" | "image" | "createdAt" | "modifiedAt"
+  > = {
     id: 201,
     title: "Wuthering Heights",
     descr:
       "Wuthering Heights, Emily Brontë's only novel, was published in 1847 under the pseudonym \"Ellis Bell\". It was written between October 1845 and June 1846. Wuthering Heights and Anne Brontë's Agnes Grey were accepted by publisher Thomas Newby before the success of their sister Charlotte's novel Jane Eyre. After Emily's death, Charlotte edited the manuscript of Wuthering Heights and arranged for the edited version to be published as a posthumous second edition in 1850.",
-    author: "Emily Brontë",
-    genreId: 11,
     stock: 12,
-    price: 11.11,
+    price: "11.11",
     currencyCode: "GBP",
+    genreId: 11,
+    genre: {
+      __deferred: {
+        uri: "http://localhost:4004/v2/browse/Books(201)/genre",
+      },
+    },
   };
 
   test("string returning function with param", async () => {
@@ -21,7 +28,8 @@ describe("CAP V4 Integration Testing: Operation Capabilities", () => {
 
     expect(result.status).toBe(200);
     expect(result.data).toBeDefined();
-    expect(result.data.value).toBe("Hello Horst");
+    // a certain CAP touch here: operation name is taken as key for primitive value
+    expect(result.data.d.hello).toBe("Hello Horst");
   });
 
   test("string collection returning function", async () => {
@@ -29,7 +37,7 @@ describe("CAP V4 Integration Testing: Operation Capabilities", () => {
 
     expect(result.status).toBe(200);
     expect(result.data).toBeDefined();
-    expect(result.data.value).toStrictEqual(["Hello", "Hallo", "Holla", "Olé olé!"]);
+    expect(result.data.d.results).toStrictEqual(["Hello", "Hallo", "Holla", "Olé olé!"]);
   });
 
   test("entity type returning function", async () => {
@@ -37,7 +45,7 @@ describe("CAP V4 Integration Testing: Operation Capabilities", () => {
 
     expect(result.status).toBe(200);
     expect(result.data).toBeDefined();
-    expect(result.data).toMatchObject(BOOK_ZERO);
+    expect(result.data.d).toMatchObject(BOOK_ZERO);
   });
 
   test("entity collection returning function", async () => {
@@ -45,7 +53,7 @@ describe("CAP V4 Integration Testing: Operation Capabilities", () => {
 
     expect(result.status).toBe(200);
     expect(result.data).toBeDefined();
-    expect(result.data.value.length).toBe(5);
-    expect(result.data.value[0]).toMatchObject(BOOK_ZERO);
+    expect(result.data.d.results.length).toBe(5);
+    expect(result.data.d.results[0]).toMatchObject(BOOK_ZERO);
   });
 });
