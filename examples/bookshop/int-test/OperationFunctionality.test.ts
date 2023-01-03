032c981e598b8daeb40b91db83fa7cf4e7d39586
texttechne <gedankengut@gmx.de>
2023-01-03 23:16:37 +0100

    fix(example-cap): fix V4 test

--- examples/bookshop/int-test/OperationFunctionality.test.ts
+++ examples/bookshop/int-test/OperationFunctionality.test.ts
@@ -4,12 +4,11 @@ import { catalogService } from "./services";
 describe("CAP V4 Integration Testing: Operation Capabilities", () => {
   const testService = catalogService;
 
-  const BOOK_ZERO: Omit<BooksModel, "image" | "createdAt" | "modifiedAt"> = {
+  const BOOK_ZERO: Omit<BooksModel, "author" | "image" | "createdAt" | "modifiedAt"> = {
     id: 201,
     title: "Wuthering Heights",
     descr:
       "Wuthering Heights, Emily Brontë's only novel, was published in 1847 under the pseudonym \"Ellis Bell\". It was written between October 1845 and June 1846. Wuthering Heights and Anne Brontë's Agnes Grey were accepted by publisher Thomas Newby before the success of their sister Charlotte's novel Jane Eyre. After Emily's death, Charlotte edited the manuscript of Wuthering Heights and arranged for the edited version to be published as a posthumous second edition in 1850.",
-    author: "Emily Brontë",
     genreId: 11,
     stock: 12,
     price: 11.11,
