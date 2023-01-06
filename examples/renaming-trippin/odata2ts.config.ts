f6d73757aedd273db39ce19bc0619f54db6f479c
texttechne <gedankengut@gmx.de>
2023-01-06 19:52:34 +0100

    feat(example-renaming): Complete Example Project Demonstrating Renaming (#103)

---
+++ examples/renaming-trippin/odata2ts.config.ts
@@ -0,0 +1,84 @@
+import { ConfigFileOptions, EmitModes, Modes, NamingStrategies } from "@odata2ts/odata2ts";
+
+const config: ConfigFileOptions = {
+  debug: true,
+  mode: Modes.service,
+  emitMode: EmitModes.ts,
+  // TrippinService does not generate IDs on the server, but the client side => demo service
+  disableAutoManagedKey: true,
+  allowRenaming: true,
+  services: {
+    trippin: {
+      source: "resource/trippin.xml",
+      output: "generated-src/TRIPPIN",
+      naming: {
+        models: {
+          namingStrategy: NamingStrategies.CONSTANT_CASE,
+          propNamingStrategy: NamingStrategies.SNAKE_CASE,
+          fileName: {
+            prefix: "",
+            suffix: "types",
+            namingStrategy: NamingStrategies.CONSTANT_CASE,
+          },
+        },
+        queryObjects: {
+          prefix: "",
+          suffix: "queryObject",
+          namingStrategy: NamingStrategies.CONSTANT_CASE,
+          propNamingStrategy: NamingStrategies.SNAKE_CASE,
+          fileName: {
+            prefix: "",
+            suffix: "queryObjects",
+            namingStrategy: NamingStrategies.CONSTANT_CASE,
+          },
+        },
+        services: {
+          prefix: "service",
+          suffix: "",
+          namingStrategy: NamingStrategies.CONSTANT_CASE,
+          main: {
+            applyServiceNaming: true,
+            namingStrategy: NamingStrategies.NONE,
+            prefix: "xxx_",
+            suffix: "_xxxs",
+          },
+          privateProps: {
+            namingStrategy: NamingStrategies.SNAKE_CASE,
+            suffix: "_",
+          },
+          publicProps: {
+            namingStrategy: NamingStrategies.CONSTANT_CASE,
+          },
+          serviceResolverFunction: {
+            namingStrategy: NamingStrategies.SNAKE_CASE,
+            suffix: "RSLVR",
+          },
+          relatedServiceGetter: {
+            namingStrategy: NamingStrategies.SNAKE_CASE,
+            prefix: "navigateTo",
+            suffix: "",
+          },
+          operations: {
+            namingStrategy: NamingStrategies.SNAKE_CASE,
+            function: {
+              suffix: "Func",
+            },
+            action: {
+              suffix: "Act",
+            },
+          },
+        },
+      },
+      propertiesByName: [
+        {
+          name: "Gender",
+          mappedName: "TraditionalGenderCategories",
+          managed: true,
+        },
+        ...["createdAt", "createdBy", "modifiedAt", "modifiedBy"].map((prop) => ({ name: prop, managed: true })),
+      ],
+    },
+  },
+};
+
+export default config;
