784ae784d44491cfdb7069d4b173804146aa10e5
manuelseeger <mail@manuelseeger.de>
2023-04-17 12:36:42 +0200

    Revert "fix: refactor reordering into seperate method"
    This reverts commit 99914ec49080c578fc9157fbf9c2128c2ca69a57.

--- packages/odata2ts/src/generator/QueryObjectGenerator.ts
+++ packages/odata2ts/src/generator/QueryObjectGenerator.ts
@@ -48,8 +48,15 @@ class QueryObjectGenerator {
   }
 
   private generateModels(importContainer: ImportContainer) {
-    const modelTypes = this.reorderModelsByInheritance<ModelType>(this.dataModel.getModels());
-    modelTypes.forEach((model) => {
+    // bring models in order of inheritance (base classes first)
+    const inheritedModels = this.dataModel.getModels().filter((m) => m.baseClasses.length);
+    const baseModels = this.dataModel.getModels().filter((m) => !m.baseClasses.length);
+    for (const model of inheritedModels) {
+      let index = baseModels.findIndex((m) => m.name == model.baseClasses[0]);
+      index = index == -1 ? baseModels.length : index + 1;
+      baseModels.splice(index, 0, model);
+    }
+    baseModels.forEach((model) => {
       this.generateModel(model, importContainer);
       if (!this.options.skipIdModels) {
         this.generateIdFunction(model, importContainer);
@@ -58,26 +65,21 @@ class QueryObjectGenerator {
         this.generateBoundOperations(model.name, importContainer);
       }
     });
-    const complexTypes = this.reorderModelsByInheritance<ComplexType>(this.dataModel.getComplexTypes());
-    complexTypes.forEach((model) => {
+
+    const inheritedComplexTypes = this.dataModel.getComplexTypes().filter((m) => m.baseClasses.length);
+    const baseComplexTypes = this.dataModel.getComplexTypes().filter((m) => !m.baseClasses.length);
+    for (const model of inheritedComplexTypes) {
+      let index = baseComplexTypes.findIndex((m) => m.name == model.baseClasses[0]);
+      index = index == -1 ? baseModels.length : index + 1;
+      baseComplexTypes.splice(index, 0, model);
+    }
+    baseComplexTypes.forEach((model) => {
       this.generateModel(model, importContainer);
     });
 
     if (this.dataModel.getModels().length || this.dataModel.getComplexTypes().length) {
       importContainer.addFromQObject("QueryObject");
     }
-  }
-
-  private reorderModelsByInheritance<T extends ComplexType>(models: T[]): T[] {
-    // bring models in order of inheritance (base classes first)
-    const inheritedModels = models.filter((m) => m.baseClasses.length);
-    const baseModels = models.filter((m) => !m.baseClasses.length);
-    for (const model of inheritedModels) {
-      let index = baseModels.findIndex((m) => m.name == model.baseClasses[0]);
-      index = index == -1 ? baseModels.length : index + 1;
-      baseModels.splice(index, 0, model);
-    }
-    return baseModels;
   }
 
   private generateModel(model: ComplexType, importContainer: ImportContainer) {
