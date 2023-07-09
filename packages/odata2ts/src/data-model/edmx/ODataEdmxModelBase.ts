dce396d4540a09bc50e0e4c6fded237bd8399706
texttechne <gedankengut@gmx.de>
2023-07-09 22:23:29 +0200

    feat(odata2ts): support schema alias

--- packages/odata2ts/src/data-model/edmx/ODataEdmxModelBase.ts
+++ packages/odata2ts/src/data-model/edmx/ODataEdmxModelBase.ts
@@ -17,7 +17,7 @@ export interface Schema<ET extends EntityType, CT extends ComplexType> {
   $: {
     Namespace: string;
     xmlns: string;
-    Alias: string;
+    Alias?: string;
   };
   EntityType?: Array<ET>;
   ComplexType?: Array<CT>;
