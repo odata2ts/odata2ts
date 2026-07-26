de623ca91db044e6e4acc4eba56afbd04f35b77b
texttechne <gedankengut@gmx.de>
2026-07-26 02:03:58 +0200

    feat(odata-core): Add 4.01 types.
    Short form of meta properties, e.g. "@context" instead of "@odata.context"

--- packages/odata-core/src/index.ts
+++ packages/odata-core/src/index.ts
@@ -2,5 +2,7 @@ export * from "./ODataVersions";
 export * from "./ODataTypesV4";
 export * from "./ODataTypesV2";
 export * from "./ResponseModelV4";
+export * from "./ResponseModelV401";
 export * from "./ResponseModelV2";
 export * from "./ODataModelPayloadV4";
+export * from "./ODataModelPayloadV401";
